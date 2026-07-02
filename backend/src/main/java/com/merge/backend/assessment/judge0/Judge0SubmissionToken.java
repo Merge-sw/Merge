2f3a7298376ade5acf159a408595cdc00e7bb183
Adewole Adedotun <erik.edwards.jon@gmail.com>
2026-07-02 04:42:01 -0700

    Add Judge0SubmissionToken: holds token from POST /submissions response

---
+++ backend/src/main/java/com/merge/backend/assessment/judge0/Judge0SubmissionToken.java
@@ -0,0 +1,9 @@
+package com.merge.backend.assessment.judge0;
+
+public class Judge0SubmissionToken {
+
+    private String token;
+
+    public String getToken() { return token; }
+    public void setToken(String token) { this.token = token; }
+}
